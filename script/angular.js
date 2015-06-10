918ff7a0dfb9973565141663d620c5b6bd704cbc
eebasadre20 <eebasadre20@gmail.com>
2015-06-10 23:42:05 +0800

    Initialize app, testing swipe and add board

---
+++ script/angular.js
@@ -0,0 +1,37 @@
+var game = angular.module( "2048" , [ ] );
+
+game
+	.directive( "boardGame", [
+		"Gen2048",
+		function directive ( Gen2048 ) {
+			return {
+				"restrict": "A",
+				"scope": true,
+				"link": function onLink ( scope, element, attributeSet ) {
+					scope.tiles = [ ];
+
+					scope.$on( "start-game", function ( ) {
+						scope.$apply( function ( ) {
+							scope.tiles.push( scope.tiles.length );
+							scope.tiles.push( scope.tiles.length );
+						} );
+					} );
+
+				}
+			}
+		}
+	] );
+
+game
+	.directive ( "start", [
+		"$rootScope",
+		function directive ( $rootScope ) {
+			return function ( scope, element ) {
+				element.bind( "click" , 
+					function ( ) {
+						$rootScope.$broadcast( "start-game" );
+						element.addClass( "hidden" );
+				} );
+			}
+		}
+	] );
