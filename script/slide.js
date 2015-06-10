613a09161b35b5e6de78f7dfd5b8cf44720bde37
eebasadre20 <eebasadre20@gmail.com>
2015-06-10 14:48:46 +0800

    Initialize 2048 Game app

---
+++ script/slide.js
@@ -0,0 +1,97 @@
+$( function ( ) {	
+	var wOffset = parseInt( $( "#board" ).width( ) / 4 );
+	var wholeWidth = $( "#board" ).width( );		
+	
+	var events = [
+		{
+			"name": "swipeLeft",
+			"handler": new SwipeEvent( function ( ) {
+				if ( !$( ".tiles" ).is(":animated") )
+					$( "#board" ).trigger( "fire" , "Left" );						
+			} )
+		},
+		{
+			"name": "swipeRight",
+			"handler": new SwipeEvent( function ( ) {
+				if ( !$( ".tiles" ).is(":animated") )
+					$( "#board" ).trigger( "fire" , "Right" );						
+			} )
+		},
+		{
+			"name": "swipeUp",
+			"handler": new SwipeEvent( function ( ) {
+				if ( !$( ".tiles" ).is(":animated") )
+					$( "#board" ).trigger( "fire" , "Up" );				
+			} )
+		},
+		{
+			"name": "swipeDown",
+			"handler": new SwipeEvent( function ( ) {
+				if ( !$( ".tiles" ).is(":animated") )
+					$( "#board" ).trigger( "fire" , "Down" );				
+			} )
+		}
+	];
+
+	$( "#board" ).on( "moveLeft" , 
+		function ( evt , data ) {
+			tileMove( wOffset*-1 , 0 , data );
+		} );
+	$( "#board" ).on( "moveRight" , 
+		function ( evt , data ) {
+			tileMove( wOffset , 0 , data );
+		} );
+	$( "#board" ).on( "moveUp" , 
+		function ( evt , data ) {
+			tileMove( 0 , wOffset*-1 , data );
+		} );
+	$( "#board" ).on( "moveDown" , 
+		function ( evt , data ) {
+			tileMove( 0 , wOffset , data );
+		} );
+	$( "#board" ).on( "newTile" , 
+		function ( evt , data ) {				
+			var topOffset = parseInt( $( "#board" ).offset( ).top );
+			data.element.css( "left" , (wOffset * data.points[1]) + "px" );
+			data.element.css( "top" , (topOffset + (wOffset * data.points[0])) + "px" );
+		} );
+
+	var tileMove = function tileMove ( h , v , data ) {		
+		var left = data.element.offset( ).left;
+		var top = data.element.offset( ).top;
+			
+			data.element.animate( {
+				"left": left + (h * data.moves),
+				"top": top + (v * data.moves)
+			} , "slow" , function ( ) {
+				if ( data.handler ) {
+					data.element.addClass( "hidden" );
+				}
+			} );
+	};
+
+	var registerEvent = function registerEvent ( ) {
+		events.forEach( function ( each ) {
+			$( "#board" ).on( each.name );
+		} );		
+	};
+
+	var SwipeBoard = function SwipeBoard ( ) {
+		var swipe = { };
+		events.forEach( function ( each ) {
+			swipe[ each.name ] = each.handler;
+		} );
+
+		return swipe;
+	}
+
+	$( "#board" ).swipe( SwipeBoard( ) );	
+
+	registerEvent( );
+
+	function SwipeEvent ( handler ) {
+		return function ( event , distance , duration , fingerCount , fingerData ) {
+			handler( );
+		};
+	};
+} );
